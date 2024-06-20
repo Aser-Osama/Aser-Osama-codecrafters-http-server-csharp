8fd9c2afca4dc0e27654ce4582901f464494ed90
Aser-Osama <117906529+Aser-Osama@users.noreply.github.com>
2024-06-20 22:50:01 +0300

    task 5

--- src/Server.cs
+++ src/Server.cs
@@ -13,77 +13,83 @@ TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 Console.WriteLine($"TCP Server has started");
 // wait for client, this is blocking
-Console.WriteLine("Awaiting requests");
-Socket socket = server.AcceptSocket();
-
-
-//Receive request from server
-Byte[] recBytes = new Byte[4096];
-int r = socket.Receive(recBytes);
-string r_string = new string(Encoding.ASCII.GetChars(recBytes));
-var r_arr = r_string.Split('\n');
-var endpoint = r_arr[0].Split(' ')[1].Split('/');
-
-//Send response back to servers
-string responseStatus = "";
-string responseContent = "\r\n\r\n";
-if (endpoint[1] == "")
+while (true)
 {
-    responseStatus = "200 OK";
+    Console.WriteLine("Awaiting requests");
+    Socket socket = server.AcceptSocket();
+    Thread clientThread = new Thread(() => HandleClient(socket));
+    clientThread.Start();
 }
-else if (endpoint[1].ToLower() == "echo")
+
+
+
+static void HandleClient(Socket socket)
 {
-    responseStatus = "200 OK";
-    responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {endpoint[2].Length}\r\n\r\n{endpoint[2]}";
-}
-else if (endpoint[1].ToLower() == "user-agent")
-{
-    responseStatus = "200 OK";
-    if (r_arr.Length > 3)
+    //Receive request from server
+    Byte[] recBytes = new Byte[4096];
+    int r = socket.Receive(recBytes);
+    string r_string = new string(Encoding.ASCII.GetChars(recBytes));
+    var r_arr = r_string.Split('\n');
+    var endpoint = r_arr[0].Split(' ')[1].Split('/');
+
+    //Send response back to servers
+    string responseStatus = "";
+    string responseContent = "\r\n\r\n";
+    switch (endpoint[1].ToLower())
     {
-        string userAgentLine = "";
-        foreach (string line in r_arr)
-        {
-            if (line.StartsWith("User-Agent:"))
+        case "":
+            responseStatus = "200 OK";
+            break;
+        case "echo":
+            responseStatus = "200 OK";
+            responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {endpoint[2].Length}\r\n\r\n{endpoint[2]}";
+            break;
+        case "user-agent":
+            responseStatus = "200 OK";
+            if (r_arr.Length > 3)
             {
-                userAgentLine = line;
+                string userAgentLine = "";
+                foreach (string line in r_arr)
+                {
+                    if (line.StartsWith("User-Agent:"))
+                    {
+                        userAgentLine = line;
+                    }
+                }
+                if (string.IsNullOrEmpty(userAgentLine))
+                {
+                    responseContent = "\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
+                }
+                else
+                {
+                    string[] parts = userAgentLine.Split(':');
+                    string agent = parts[1].Trim();
+                    int contentLength = Encoding.UTF8.GetByteCount(agent);
+                    responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {contentLength}\r\n\r\n{agent}";
+                }
             }
-        }
-        if (string.IsNullOrEmpty(userAgentLine))
-        {
-            responseContent = "\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
-        }
-        else
-        {
-            string[] parts = userAgentLine.Split(':');
-            string agent = parts[1].Trim();
-            int contentLength = Encoding.UTF8.GetByteCount(agent);
-            responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {contentLength}\r\n\r\n{agent}";
-        }
+            else
+            {
+                responseContent = "\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
+            }
+            break;
 
+        default:
+            responseStatus = "404 Not Found";
+            break;
     }
-    else
+
+    string responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
+    Byte[] sendBytes = Encoding.ASCII.GetBytes(responseString);
+    int totalBytesSent = 0;
+    while (totalBytesSent < sendBytes.Length)
     {
-        responseContent = "\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
+        int sent = socket.Send(sendBytes, totalBytesSent, sendBytes.Length - totalBytesSent, SocketFlags.None);
+        if (sent == 0)
+        {
+            break; // Socket has been closed or an error occurred.
+        }
+        totalBytesSent += sent;
     }
-
+    Console.WriteLine("Sent: \n------------\n" + responseString + "\n------------\n");
 }
-else
-{
-    responseStatus = "404 Not Found";
-}
-
-string responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
-Byte[] sendBytes = Encoding.ASCII.GetBytes(responseString);
-int totalBytesSent = 0;
-while (totalBytesSent < sendBytes.Length)
-{
-    int sent = socket.Send(sendBytes, totalBytesSent, sendBytes.Length - totalBytesSent, SocketFlags.None);
-    if (sent == 0)
-    {
-        break; // Socket has been closed or an error occurred.
-    }
-    totalBytesSent += sent;
-}
-Console.WriteLine("Sent: \n------------\n" + responseString + "\n------------\n");
-
