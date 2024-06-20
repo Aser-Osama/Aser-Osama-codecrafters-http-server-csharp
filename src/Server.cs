4d9d38a4e97072d984903d74d12702b2e791be6c
Aser-Osama <117906529+Aser-Osama@users.noreply.github.com>
2024-06-20 21:02:18 +0300

    task 4

--- src/Server.cs
+++ src/Server.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.WebSockets;
@@ -12,22 +13,23 @@ server.Start();
 Console.WriteLine($"TCP Server has started");
 while (true)
 {
-    Console.WriteLine("Awaiting requests");
     // wait for client, this is blocking
+    Console.WriteLine("Awaiting requests");
     Socket socket = server.AcceptSocket();
 
-    //Log the connection URL
 
     //Receive request from server
     Byte[] recBytes = new Byte[512];
     int r = socket.Receive(recBytes);
     string r_string = new string(Encoding.ASCII.GetChars(recBytes));
-    var endpoint = r_string.Split('\n')[0].Split(' ')[1].Split('/');
-    foreach (var item in endpoint)
-    {
-        Console.WriteLine($"|{item}|");
-    }
-
+    var r_arr = r_string.Split('\n');
+    var endpoint = r_arr[0].Split(' ')[1].Split('/');
+    // Console.WriteLine(r_string + "\n ------------");
+    // foreach (var item in endpoint)
+    // {
+    //     Console.Write($"|{item}|");
+    // }
+    // Console.WriteLine("");
 
     //Send response back to servers
     string responseStatus = "";
@@ -40,6 +42,17 @@ while (true)
     {
         responseStatus = "200 OK";
         responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {endpoint[2].Length}\r\n\r\n{endpoint[2]}";
+    }
+    else if (endpoint[1].ToLower() == "user-agent")
+    {
+        responseStatus = "200 OK";
+        if (r_arr.Length >= 6)
+        {
+            //Console.WriteLine(r_arr[3]);
+            string agent = r_arr[3].Split(':')[1][1..];
+            //Console.WriteLine(agent);
+            responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {agent.Length}\r\n\r\n{agent}";
+        }
     }
     else
     {
