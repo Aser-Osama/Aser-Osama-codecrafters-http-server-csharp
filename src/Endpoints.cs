e93aeb30352dce6b7a8eed7f3bcb723f0ddd452c
Aser-Osama <117906529+Aser-Osama@users.noreply.github.com>
2024-06-23 02:03:38 +0300

    codecrafters submit [skip ci]

---
+++ src/Endpoints.cs
@@ -0,0 +1,134 @@
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+
+public class Endpoints
+{
+    public string EndpointURL { get; }
+    public string HTTP_Verb { get; }
+    string[] RequestArray;
+    string[] FullEndpointURL;
+    string FilePath;
+    string r_string;
+    public Endpoints(Byte[] recBytes, string filepath)
+    {
+        r_string = new string(Encoding.ASCII.GetChars(recBytes));
+        RequestArray = r_string.Split('\n');
+        var endpoint_line = RequestArray[0].Split(' ');
+        FullEndpointURL = endpoint_line[1].Split('/');
+        EndpointURL = FullEndpointURL[1];
+        HTTP_Verb = endpoint_line[0];
+        FilePath = filepath;
+    }
+    public string getIndex()
+    {
+        string responseContent = "\r\n\r\n";
+        string responseStatus = "200 OK";
+        string responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
+        return responseString;
+    }
+    public string getEcho()
+    {
+        string encodingType = "";
+        foreach (var ReqLine in RequestArray)
+        {
+
+            if (ReqLine.ToLower().Contains("encoding"))
+            {
+                var compAlg = ReqLine.Split(':')[1].Trim();
+                Console.WriteLine(compAlg);
+                if (compAlg == "gzip")
+                {
+                    encodingType = "\r\nContent-Encoding: gzip";
+                    break;
+                }
+            }
+        }
+        string responseStatus = "200 OK";
+        string responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {FullEndpointURL[2].Length}\r\n\r\n{FullEndpointURL[2]}";
+        string responseString = $"HTTP/1.1 {responseStatus}{encodingType}{responseContent}";
+        return responseString;
+    }
+    public string getUserAgent()
+    {
+
+        string responseStatus = "200 OK";
+        string responseContent;
+        if (RequestArray.Length <= 3)
+        {
+            responseStatus = "404 Not Found";
+            responseContent = "\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
+            string responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
+            return responseString;
+        }
+        string userAgentLine = "";
+        foreach (string line in RequestArray)
+        {
+            if (line.StartsWith("User-Agent:"))
+            {
+                userAgentLine = line;
+                break;
+            }
+        }
+        if (string.IsNullOrEmpty(userAgentLine))
+        {
+            responseContent = "\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
+            string responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
+            return responseString;
+        }
+        else
+        {
+            string[] parts = userAgentLine.Split(':');
+            string agent = parts[1].Trim();
+            int contentLength1 = Encoding.UTF8.GetByteCount(agent);
+            responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {contentLength1}\r\n\r\n{agent}";
+            string responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
+            return responseString;
+        }
+    }
+    public string getFiles()
+    {
+
+        string responseContent = "\r\n\r\n";
+        string responseStatus;
+        string responseString;
+        Console.WriteLine(FilePath + FullEndpointURL[2]);
+        if (string.IsNullOrEmpty(FilePath) || string.IsNullOrEmpty(FullEndpointURL[2]) || !File.Exists(FilePath + FullEndpointURL[2]))
+        {
+            responseStatus = "404 Not Found";
+            responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
+            return responseString;
+        }
+        string fileContent = File.ReadAllText(FilePath + FullEndpointURL[2]);
+        int contentLength2 = Encoding.UTF8.GetByteCount(fileContent);
+        responseStatus = "200 OK";
+        responseContent = $"\r\nContent-Type: application/octet-stream\r\nContent-Length: {contentLength2}\r\n\r\n{fileContent}";
+        responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
+        return responseString;
+    }
+    public string postFiles()
+    {
+
+        string responseContent = "\r\n\r\n";
+        string responseStatus;
+        string responseString;
+
+        Console.WriteLine(FilePath + FullEndpointURL[2]);
+        if (string.IsNullOrEmpty(FilePath) || string.IsNullOrEmpty(FullEndpointURL[2]))
+        {
+            responseStatus = "404 Not Found";
+            responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
+            return responseString;
+        }
+        string filePath = FilePath + FullEndpointURL[2];
+        var data = RequestArray[RequestArray.Length - 1].Trim('\0').Trim();
+        Console.WriteLine(data.Length);
+        File.WriteAllText(filePath, data);
+        responseStatus = "201 Created";
+        responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
+        return responseString;
+    }
+}
--- src/Server.cs
+++ src/Server.cs
@@ -9,10 +9,7 @@ public class MainClass
 {
     public static void Main(string[] args)
     {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        //Console.WriteLine("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
         TcpListener server = new TcpListener(IPAddress.Any, 4221);
         server.Start();
         Console.WriteLine($"TCP Server has started");
@@ -37,88 +34,34 @@ public class MainClass
         //Receive request from server
         Byte[] recBytes = new Byte[4096];
         int r = socket.Receive(recBytes);
-        string r_string = new string(Encoding.ASCII.GetChars(recBytes));
-        var r_arr = r_string.Split('\n');
-        var endpoint_line = r_arr[0].Split(' ');
-        var endpoint = endpoint_line[1].Split('/');
-        var http_verb = endpoint_line[0];
-
+        Endpoints ep = new Endpoints(recBytes, fp);
+        string endpoint = ep.EndpointURL;
+        string HTTP_Verb = ep.HTTP_Verb;
         //Send response back to servers
-        string responseStatus = "";
-        string responseContent = "\r\n\r\n";
-        switch (endpoint[1].ToLower())
+        string responseString = "HTTP/1.1 404 Not Found\r\n\r\n"; ;
+
+        switch (endpoint)
         {
             case "":
-                responseStatus = "200 OK";
+                responseString = ep.getIndex();
                 break;
             case "echo":
-                responseStatus = "200 OK";
-                responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {endpoint[2].Length}\r\n\r\n{endpoint[2]}";
+                responseString = ep.getEcho();
+
                 break;
             case "user-agent":
-                responseStatus = "200 OK";
-                if (r_arr.Length <= 3)
-                {
-                    responseContent = "\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
-                    break;
-                }
-                string userAgentLine = "";
-                foreach (string line in r_arr)
-                {
-                    if (line.StartsWith("User-Agent:"))
-                    {
-                        userAgentLine = line;
-                    }
-                }
-                if (string.IsNullOrEmpty(userAgentLine))
-                {
-                    responseContent = "\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
-                }
-                else
-                {
-                    string[] parts = userAgentLine.Split(':');
-                    string agent = parts[1].Trim();
-                    int contentLength1 = Encoding.UTF8.GetByteCount(agent);
-                    responseContent = $"\r\nContent-Type: text/plain\r\nContent-Length: {contentLength1}\r\n\r\n{agent}";
-                }
+                responseString = ep.getUserAgent();
                 break;
             case "files":
-                if (http_verb.ToLower() == "get")
-                {
-
-                    Console.WriteLine(fp + endpoint[2]);
-                    if (string.IsNullOrEmpty(fp) || string.IsNullOrEmpty(endpoint[2]) || !File.Exists(fp + endpoint[2]))
-                    {
-                        responseStatus = "404 Not Found";
-                        break;
-                    }
-                    responseStatus = "200 OK";
-                    string fileContent = File.ReadAllText(fp + endpoint[2]);
-                    int contentLength2 = Encoding.UTF8.GetByteCount(fileContent);
-                    responseContent = $"\r\nContent-Type: application/octet-stream\r\nContent-Length: {contentLength2}\r\n\r\n{fileContent}";
-                }
-                else if (http_verb.ToLower() == "post")
-                {
-                    Console.WriteLine(fp + endpoint[2]);
-                    if (string.IsNullOrEmpty(fp) || string.IsNullOrEmpty(endpoint[2]))
-                    {
-                        responseStatus = "404 Not Found";
-                        break;
-                    }
-                    string filePath = fp + endpoint[2];
-                    var data = r_arr[r_arr.Length - 1].Trim('\0').Trim();
-                    Console.WriteLine(data.Length);
-                    File.WriteAllText(filePath, data);  // Create a file and write the content of writeText to it
-                    responseStatus = "201 Created";
-                }
+                if (HTTP_Verb == "GET")
+                    responseString = ep.getFiles();
+                else if (HTTP_Verb == "POST")
+                    responseString = ep.postFiles();
                 break;
-
             default:
-                responseStatus = "404 Not Found";
                 break;
         }
 
-        string responseString = $"HTTP/1.1 {responseStatus}{responseContent}";
         Byte[] sendBytes = Encoding.ASCII.GetBytes(responseString);
         int totalBytesSent = 0;
         while (totalBytesSent < sendBytes.Length)
@@ -132,4 +75,6 @@ public class MainClass
         }
         Console.WriteLine("Sent: \n------------\n" + responseString + "\n------------\n");
     }
+
+
 }
